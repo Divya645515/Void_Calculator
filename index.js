45c44febd6732a1eced50f44bf7cc3ba89601cbc
Rohithashwa <rohangangodi@gmail.com>
2025-05-21 11:24:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-let h2
+let display = document.getElementById("display");
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
+
+let plus  = document.getElementById("plus");
+let minus = document.getElementById("minus");
+let mul = document.getElementById("mul");
+
